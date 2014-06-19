7f83cc525b290813ec1c721b72b61b5b6ec5efef
Udara <y.b.n.udara@gmail.com>
2014-06-19 20:11:04 +0530

    update provider

--- src/main/java/org/apache/airavata/gfac/jclouds/provider/impl/JCloudsProvider.java
+++ src/main/java/org/apache/airavata/gfac/jclouds/provider/impl/JCloudsProvider.java
@@ -20,16 +20,21 @@
 */
 package org.apache.airavata.gfac.jclouds.provider.impl;
 
+import org.apache.airavata.commons.gfac.type.ActualParameter;
 import org.apache.airavata.commons.gfac.type.ApplicationDescription;
+import org.apache.airavata.commons.gfac.type.MappingFactory;
 import org.apache.airavata.gfac.GFacException;
 import org.apache.airavata.gfac.core.context.JobExecutionContext;
+import org.apache.airavata.gfac.core.context.MessageContext;
 import org.apache.airavata.gfac.core.provider.AbstractProvider;
 import org.apache.airavata.gfac.core.provider.GFacProviderException;
 import org.apache.airavata.gfac.jclouds.utils.JCloudsFileTransfer;
+import org.apache.airavata.gfac.jclouds.utils.JCloudsUtils;
 import org.apache.airavata.schemas.gfac.Ec2ApplicationDeploymentType;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 
+import org.jclouds.compute.domain.ExecResponse;
 import org.jclouds.domain.LoginCredentials;
 import static com.google.common.collect.Iterables.getOnlyElement;
 
@@ -38,6 +43,7 @@ import org.slf4j.LoggerFactory;
 import org.apache.airavata.gfac.jclouds.security.JCloudsSecurityContext;
 
 import java.util.Map;
+import java.util.Set;
 
 public class JCloudsProvider extends AbstractProvider {
     private static final Logger log = LoggerFactory.getLogger(JCloudsProvider.class);
@@ -49,20 +55,47 @@ public class JCloudsProvider extends AbstractProvider {
     private ComputeService service;
     private ComputeServiceContext context;
     private LoginCredentials credentials;
+    private JCloudsUtils jCloudsUtils;
+
 
     public void initialize(JobExecutionContext jobExecutionContext) throws GFacException, GFacProviderException {
+        if(jobExecutionContext==null) {
+            throw new GFacException("jobexecution context is null");
+        }
         super.initialize(jobExecutionContext);
-        JCloudsFileTransfer fileTransfer=new JCloudsFileTransfer(context,nodeId,credentials);
+        jCloudsUtils=JCloudsUtils.getInstance();
+        credentials=jCloudsUtils.getCredentials();
     }
-
-    @Override
-    public void initProperties(Map<String, String> properties) throws GFacProviderException, GFacException {
-    }
-
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws GFacProviderException, GFacException {
+        String command=buildCommand(jobExecutionContext);
+        ExecResponse response=jCloudsUtils.runScriptOnNode(credentials,command,true);
+        
+    }
+
+
+    public String buildCommand(JobExecutionContext context){
+        ApplicationDescription ec2App=jobExecutionContext.getApplicationContext().getApplicationDeploymentDescription();
+        Ec2ApplicationDeploymentType app=(Ec2ApplicationDeploymentType)ec2App.getType();
+        String SPACE=" ";
+        StringBuffer cmd=new StringBuffer();
+        cmd.append(app.getExecutableType());
+        cmd.append(SPACE);
+        cmd.append(app.getExecutableLocation());
+
+        MessageContext input=context.getInMessageContext();
+        Map<String, Object> inputs = input.getParameters();
+        Set<String> keys = inputs.keySet();
+        for(String paraName :keys){
+            ActualParameter actualParameter=(ActualParameter)input.getParameter(paraName);
+            String paramValue=MappingFactory.toString(actualParameter);
+            cmd.append(paramValue);
+            cmd.append(SPACE);
+        }
+        return cmd.toString();
+
     }
 
     @Override
@@ -73,16 +106,7 @@ public class JCloudsProvider extends AbstractProvider {
     public void cancelJob(String jobId, JobExecutionContext jobExecutionContext) throws GFacProviderException, GFacException {
     }
 
-    public void buildCommand(JobExecutionContext context1){
-        ApplicationDescription ec2App=jobExecutionContext.getApplicationContext().getApplicationDeploymentDescription();
-        Ec2ApplicationDeploymentType app=(Ec2ApplicationDeploymentType)ec2App.getType();
-        String SPACE=" ";
-        StringBuffer cmd=new StringBuffer();
-        cmd.append(app.getExecutableType());
-
-        cmd.append(app.getExecutableLocation());
-
-
-
+    @Override
+    public void initProperties(Map<String, String> properties) throws GFacProviderException, GFacException {
     }
 }
