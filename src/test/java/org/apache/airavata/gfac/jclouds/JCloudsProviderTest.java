cf8141bd4a4c218c7c7397e5d347f14624329672
Udara <y.b.n.udara@gmail.com>
2014-06-22 23:03:09 +0530

    remove security details

--- src/test/java/org/apache/airavata/gfac/jclouds/JCloudsProviderTest.java
+++ src/test/java/org/apache/airavata/gfac/jclouds/JCloudsProviderTest.java
@@ -50,13 +50,13 @@ public class JCloudsProviderTest {
     private String userName = "ec2-user";
 
     /* Secret key used to connect to the image */
-    private String secretKey = "10VE/FvtTuXtehmw/+buEzk3nrS+Kc4uiX+setW+";
+    private String secretKey = "";
 
     /* Access key used to connect to the image */
-    private String accessKey = "AKIAJ3M3FUZ7PTDQP4YQ";
+    private String accessKey = "";
 
     /* Instance id of the running instance of your image */
-    private String instanceId = "i-b9803a92";
+    private String instanceId = "";
 
     private String hostName="ec2";
     private String hostAddress="";
